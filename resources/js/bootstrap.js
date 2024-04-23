db01b1b817e828b2fb2dd95b28b86b32f2783b35
Colin DeCarlo <colin.decarlo@gmail.com>
2024-04-23 00:27:16 -0400

    remove commented code

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,16 +6,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 import './autobahn.js';
 window.ab = ab;
-
-// window.conn = new ab.Session('ws://localhost:8080',
-//     function() {
-//         conn.subscribe('kittensCategory', function(topic, data) {
-//             // This is where you would add the new article to the DOM (beyond the scope of this tutorial)
-//             console.log('New article published to category "' + topic + '" : ' + data.title);
-//         });
-//     },
-//     function() {
-//         console.warn('WebSocket connection closed');
-//     },
-//     {'skipSubprotocolCheck': true}
-// );
